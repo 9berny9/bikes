705da6b90a7bb2e7779cbae51d57037412ca94b8
9berny9 <bernatik.michal@gmail.com>
2022-04-27 18:07:39 +0200

    select station

--- app.py
+++ app.py
@@ -11,10 +11,34 @@ def load_data():
 
 def head_app(data_station):
     st.title("Bikes in Edinburgh")
-    st.write(f"We have {len(data_station.index)} stations in Edinburgh.")
+    st.write(f"We have {len(data_station.index)} bike stations in Edinburgh.")
     st.map(data_station)
-    st.selectbox(f"Select your station for more info:",
-                 data_station.start_station_name.sort_values())
+    station_names = data_station.name.drop_duplicates()
+    station = st.selectbox(f"Select your station for more info:",
+                           station_names)
+    if station:
+        selected_df = data_station[
+            data_station.name == station
+            ].reset_index()[["description", "lat", "lon"]]
+        if len(selected_df.index) > 1:
+            st.markdown(f"Selected station has {len(selected_df.index)} "
+                        f"habitats")
+            st.markdown("### Stations description:")
+            for i, value in enumerate(selected_df.description):
+                for n, lat in enumerate(selected_df.lat):
+                    for o, lon in enumerate(selected_df.lon):
+                        if i == n and n == o:
+                            st.write(f"The {station} is near <b>{value}</b>",
+                                     unsafe_allow_html=True)
+                            st.write(f"- Latitude: {lat}")
+                            st.write(f"- Longitude: {lon}")
+        else:
+            st.markdown("### Station description:")
+            st.write(f"The {station} station is near "
+                     f"<b>{selected_df.loc[0].description}</b>",
+                     unsafe_allow_html=True)
+            st.write(f"- Latitude: {selected_df.loc[0].lat}")
+            st.write(f"- Longitude: {selected_df.loc[0].lon}")
 
 
 def run_app():
@@ -24,15 +48,18 @@ def run_app():
 
 
 def bike_stations(data):
-    df_stations = data.drop_duplicates('start_station_name').sort_values(
+    df_stations = data.drop_duplicates('start_station_id').sort_values(
         by="start_station_name")
     df_stations = df_stations.drop(
         columns=["index", "started_at", "ended_at", "duration",
                  "end_station_id", "end_station_name",
                  "end_station_description", "end_station_latitude",
-                 "end_station_longitude"]).reset_index()
+                 "end_station_longitude"])
     df_stations = df_stations.rename(
-        columns={"start_station_latitude": "lat",
+        columns={"start_station_name": "name",
+                 "start_station_id": "id",
+                 "start_station_description": "description",
+                 "start_station_latitude": "lat",
                  "start_station_longitude": "lon"})
     return df_stations
 
