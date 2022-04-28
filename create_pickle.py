c43e5aa130c8e6fa79949d142de526b2a9c02423
Michal B <96014046+9berny9@users.noreply.github.com>
2022-04-28 14:33:04 +0200

    Delete create_pickle.py

--- create_pickle.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-import pandas as pd
-
-engine = create_engine("mysql+pymysql://student2:eh2BjVEpYmDcT96E@data.engeto.com:3306/data_academy_02_2022")
-query = 'SELECT * FROM edinburgh_bikes'
-
-df = pd.read_sql(sql=query, con=engine)
-df.to_pickle("a_file.pkl")
